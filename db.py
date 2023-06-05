3327f79e0377316ba7d8c7542983d7953d79fdbd
5YO! <115866546+petyo-petkov@users.noreply.github.com>
2023-06-05 08:25:17 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 cliente = MongoClient(
-    "mongodb+srv://loto:loto@loto.tolwpbn.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb://localhost:27017"
 )
 
 db = cliente.pruebas
